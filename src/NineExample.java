816d9f4847e61c02fc32fc28c0612fe769aca50b
KedBor <e.susha@bk.ru>
2022-02-21 12:43:14 +0300

    Add Home

--- src/NineExample.java
+++ src/NineExample.java
@@ -4,11 +4,25 @@ public class NineExample {
         int u = 1;
         int y;
         System.out.print(t + " " + u + " ");
-        for (int i = 3; i <= 20; i++) {
+        for (int i = 1; i <= 20; i++) {
             y = t + u;
             System.out.print(y + " ");
             t = u;
             u = y;
-        }
+        }System.out.println();
+
+        int z = 0;
+        int x = 1;
+        int v = 1;
+        int c;
+        System.out.print(z + " " + x + " ");
+        while(v <= 20) {
+            v++;
+            c = z + x;
+            System.out.print(c + " ");
+            z = x;
+            x = c;
+        }System.out.println();
+
     }
 }
