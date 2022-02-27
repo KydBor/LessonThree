181a7f52b543a79e363fa7c5c503c51a8d0e2ff0
KydBor <e.susha@bk..ru>
2022-02-28 01:14:24 +0300

    Add Home

--- src/TenExemple.java
+++ src/TenExemple.java
@@ -4,48 +4,18 @@ public class TenExemple {
     public static void main(String[] args) {
 //10.Выведите на экран все положительные делители натурального числа
         int n;
-        int count = 0;
+        int m = 0;
         System.out.print("Вводите ваше натуральное число - ");
         Scanner sr = new Scanner(System.in);
         n = sr.nextInt();
         System.out.print("Число " + n + " делится на - " );
         for(int i = 1; i <= n; i++){
             if ((n % i) == 0){
-                count++;
+                m++;
                 System.out.print(i + " ");
             }
         }
-        System.out.println("Количество делителей - " + count);
+        System.out.println("Количество делителей - " + m);
 
-
-
-
-
-
-
-
-
-
-/*11.Определить, является ли число простым
-            int temp;
-            boolean isPrime=true;
-            Scanner scan= new Scanner(System.in);
-            System.out.println("Введите число для проверки:");
-            // считывает число
-            int num=scan.nextInt();
-            // проверяем число на простоту
-            for (int i=2; i<=num/2; i++) {
-                temp = num % i;
-                if (temp == 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
-            // если переменная isPrime равна true, то число простое
-            if(isPrime) {
-                System.out.println(num + " - простое число");
-            } else {
-                System.out.println(num + " - составное число");
-            }*/
     }
 }
